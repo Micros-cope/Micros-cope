017d1d0f0caa8e54a463d3ab50fcbec86cc367f0
yhs <yanhaosen@yd.com.cn>
2023-02-16 13:05:49 +0800

    重新设置路由代码

--- src/main.js
+++ src/main.js
@@ -3,16 +3,18 @@ import ElementUI from 'element-ui';   // 引入element插件
 import 'element-ui/lib/theme-chalk/index.css';  // 引入element样式
 import App from './App.vue'
 import VueRouter from 'vue-router'	// 引入VueRouter
-import router from './router/index'	// 引入路由器
+import routes from './router/index'	// 引入路由器
 import store from './store'			//引入vuex
 import axios from 'axios'
 Vue.config.productionTip = false
 Vue.use(ElementUI)  // 应用element插件
-Vue.use(VueRouter)	// 应用插件
+Vue.use(VueRouter)  // 应用VueRouter插件
+const router = new VueRouter({
+    routes
+  });
 Vue.prototype.$axios = axios
 new Vue({
-	el:'#app',
-	render: h => h(App),
 	router,
 	store,
-})
+	render: h => h(App),
+}).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,70 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomePage from '../pages/HomePage/HomePage'
-import TweakCube from '../pages/GamePage/TweakCube'
-import UserManagement from '../pages/usermanagement/UserManagement'
-import nodeArt from '../pages/wordPress/nodeArt'
-import vueArt from '../pages/wordPress/vueArt'
-// 引入组件
-Vue.use(VueRouter)
-// 创建并暴露一个路由器
-export default new VueRouter({
-	routes:[
-        {
-            name:'HomePage',
-            path:'/HomePage',
-            component:HomePage
-        },
-        {
-            name:'TweakCube',
-            path:'/TweakCube',
-            component:TweakCube
-        },
-        {
-            name:'UserManagement',
-            path:'/UserManagement',
-            component:UserManagement
-        },
-        {
-            name:'nodeArt',
-            path:'/nodeArt',
-            component:nodeArt
-        },
-        {
-            name:'vueArt',
-            path:'/vueArt',
-            component:vueArt
-        }
-    ]
-})
+// import Login from '../pages/Login.vue'
+const HomePage = () => import(/* webpackChunkName:'HomePage'*/"../pages/HomePage/HomePage.vue"); 
+const TweakCube = () => import(/* webpackChunkName:'TweakCube'*/'../pages/GamePage/TweakCube.vue');
+const UserManagement = () => import(/* webpackChunkName:'UserManagement'*/'../pages/usermanagement/UserManagement.vue');
+const nodeArt = () => import(/* webpackChunkName:'nodeArt'*/'../pages/wordPress/nodeArt.vue');
+const vueArt = () => import(/* webpackChunkName:'vueArt'*/'../pages/wordPress/vueArt.vue');
+let routes = [
+    // {
+    //     name:'Login',
+    //     path: '/login',
+    //     component: Login
+    // },
+    {
+        name:'HomePage',
+        path:'/HomePage',
+        label: "首页",
+        icon: "s-home",
+        url: "Home/Home",
+        component:HomePage
+    },
+    {
+        name: "GamePage",
+        path:'/',
+        label: "个人游戏",
+        icon: "video-play",
+        children: [
+            {
+                name: "TweakCube",
+                path: "/TweakCube",
+                label: "3D魔方",
+                icon: "setting",
+                url: "GamePage/TweakCube",
+                component:TweakCube
+            },
+        ]
+    },
+    {
+        name:'UserManagement',
+        path:'/UserManagement',
+        label: "个人作品",
+        icon: "user",
+        url: "UserManage/UserManage",
+        component:UserManagement
+    },
+    {
+        name: "wordPress",
+        path:'/',
+        label: "技术分享",
+        icon: "location",
+        children: [
+            {
+                path: "/nodeArt",
+                name: "nodeArt",
+                label: "node",
+                icon: "setting",
+                url: "wordPress/nodeArt",
+                component:nodeArt
+            },
+            {
+                path: "/vueArt",
+                name: "vueArt",
+                label: "vue",
+                icon: "setting",
+                url: "wordPress/vueArt",
+                component:vueArt
+            },
+        ]
+    }
+]
+export default routes
